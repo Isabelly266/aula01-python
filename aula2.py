0d89704ff0068ceec274ad18b4a62f8fa5ab5988
Isabelly266 <isabellymatsuura@gmail.com>
2026-08-21 11:07:16 -0300

    Add student information and print statements

--- aula2.py
+++ aula2.py
@@ -1,1 +1,33 @@
-
+nome = "Isabelly"
+idade = 18
+print(nome)
+print(idade)
+print("Nome:", nome)
+print("Idade:", idade)
+nome = "Isabelly"
+idade = 18
+print(f"Meu nome é {nome} e tenho {idade} anos.")
+print("Bom dia, Giovana! \nTudo bem? \nBem vinda à Puc.")
+print("Nome\tNota")
+print("Isabelly\t9.5")
+print("Letícia\t9.5")
+print("03", "10", "2007", sep="/")
+print("Hello,", end=" ")
+print("Laura")
+nome = "Isabelly"
+idade = 18
+curso = "Administração"
+nota = 10
+print("===== FICHA DO ALUNO =====")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Curso:", curso)
+print("Nota:", nota)
+print("=============================")
+nome = "Isabelly"
+nota1 = 8.5
+nota2 = 10
+print("Aluno:", nome)
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Média:", (nota1 + nota2) / 2)
