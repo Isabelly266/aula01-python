eaa7084456ab83170ad05e667fdb2533c1508738
Isabelly266 <isabellymatsuura@gmail.com>
2026-08-28 11:04:46 -0300

    Update aula3.py

--- aula3.py
+++ aula3.py
@@ -1,1 +1,28 @@
+nota = 10
+nota_final = nota + 5
+print(nota_final)
+numero1 = 12
+numero2 = 12
+soma = numero1 + numero2
+print(soma)
+nota1 = 3
+nota2 = 5
+media = (nota1 + nota2) / 2
+print(f"Média: {media}")
+nome = input("Qual é o seu nome?: ")
+print(f"Olá, {nome}!")
 
+idade = input("Digite sua idade: ")
+print(type(idade))
+data = input("Qual é a sua data de nascimento?: ")
+print(type(data))
+
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+soma = numero_1 + numero_2
+print(f"Resultado: {soma}")
+
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
